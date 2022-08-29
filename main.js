0a796117be18139abe6e7edc35954bbb6321f0ab
rovil89 <rovil89@hotmail.com>
2022-08-29 20:24:34 -0300

    Creando el algritmo a entregar

--- main.js
+++ main.js
@@ -1,20 +1,27 @@
-/*
-alert( "Desafio Complementario");
-let nombre = prompt ("Mejor Jugador del Mundo: (para salir escribi cr7)");
-while (nombre != "cr7"){console.log(nombre);
-nombre= prompt ("Mejor Jugador del Mundo: (para salir escribi cr7)");}
-*/
+console.log("Ahorristas queriendo verdolagas")
+
+let pesos = parseInt(prompt(" Cantidad de Pesos: "))
+let valorDolar = parseInt(prompt( "Valor Dolar: "))
+
+let promedio = (pesos / valorDolar) ; 
+alert( "Con esos pesos te compras: " + promedio + " dolares")
+
+if (promedio < 100){alert ("Buen inicio pequeño ahorrista");}
+else if (promedio >= 100 && promedio < 150){ alert ("Muy bien ahorro");}
+else if (promedio >= 150 && promedio <= 200){ alert("Lindo ese ahorro Ricardo Fort")}
+else {alert ("Podes comprar hasta 200 dolares crack");}
 
 
-console.log ("G.O.A.T.: ");
-let passwordUsuario = prompt("Mejor Jugador del Mundo: ");
-const passwordAutorizado = "messi";
 
-for(let i=0 ;i<2 ; i++) {
-    if(passwordUsuario ==passwordAutorizado) {
-        alert("claro que seeeee");
-        break;}
-        else{ passwordUsuario = prompt ("Sin querer escribiste otro nombre en lugar de Messi");
-    }
-}
+
+
+
+
+
+
+
+
+
+
+
 
