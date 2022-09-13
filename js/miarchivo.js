bd76c4aa638c2f1282515a7e0fde8976d6b26957
rovil89 <rovil89@hotmail.com>
2022-09-13 15:53:45 -0300

    consulta, alta, baja, modificacion y salir

--- js/miarchivo.js
+++ js/miarchivo.js
@@ -1,41 +1,114 @@
-alert ("Se viene el mundial de Qatar, asique nada mas lindo que ver a tu seleccion en un SAMSUNG Clase Neo QLED 8K 64x valuada en US$6.500. Cuanto saldria en tu pais? y si lo haces en cuotas?")
+// Conmebol en Qatar 2022
 
-let pais = prompt("De que pais vas a ver a tu seleccion? (Arg, Bra, Uru, Ecu o Chi)");
-let cuotas = parseInt (prompt ("En cuantas cuotas lo pagarias?"));
+class Conmebol {
+    constructor (pais, primero, segundo, tercero) {
+        this.pais = pais;
+        this.primero = primero;
+        this.segundo = segundo;
+        this.tercero = tercero;
+    }
+}
 
-let dolarpaises = [" Dolar Arg $285 "," Dolar Bra R5.17 ", " Dolar Uru $40.81", " Dolar Ecu US$1 ", " Dolar Chile $909.5 " ];
+const Ecuador = new Conmebol ("Ecuador", "Qatar ", "Paises Bajos", "Senegal");
+const Argentina = new Conmebol ("Argentina", "Arabia Saudita", "Mexico", "Polonia");
+const Brasil = new Conmebol ("Brasil", "Serbia", "Suiza", "Camerun");
+const Uruguay = new Conmebol ("Uruguay", "Corea del Sur", "Portugal", "Ghana");
 
-let chileNoVaAlMundial = dolarpaises.pop();
+const arrayConmebol =[];
+
+arrayConmebol.push(Ecuador);
+arrayConmebol.push(Argentina);
+arrayConmebol.push(Brasil);
+arrayConmebol.push(Uruguay);
+
+console.log(arrayConmebol);
 
 
-while(pais != "Arg" && pais != "Bra" && pais != "Uru" && pais != "Ecu" && pais != "Chi"){pais = prompt("De que pais vas a ver a tu seleccion? (Arg, Bra, Uru, Ecu o Chi)");}
+//Menu de opciones
 
-switch(pais){
-    case "Arg":
-        alert("Pagarías "+ cuotas + " cuotas de $" + parseFloat( ((6500*285)*1.21)/cuotas).toFixed(2)+ " pesos argentinos") ;
+function menu() {
+    alert("Fase de grupo esquipos sudamericanos");
+    let opcion = parseInt (prompt ("De que pais queres saber la fase de grupos?: \n (Argentina, Ecuador,  Brasil, Uruguay) \n 1)Consulta grupo \n 2)Alta Pais \n 3)Baja Pais \n 4)Modifica un grupo \n 5)Salir"));
+    return opcion;
+}
+
+//Consulta Argentina
+
+function consultaPais(){
+    let pais = prompt ("Ingrese pais del cual quiere saber su grupo: ")
+    let Conmebol = arrayConmebol.find (Conmebol => Conmebol.pais === pais);
+    console.log(Conmebol);
+}
+
+//Agregar algun otro pais
+
+function altaPais () {
+    let pais = prompt ("Ingrese Pais: ");
+    let primero = prompt ("Debuta contra: ");
+    let segundo = prompt ("Segundo partido vs: ");
+    let tercero = prompt ("Tercer partido vs");
+    let conmebol = new Conmebol(pais, primero, segundo, tercero);
+    arrayConmebol.push(conmebol);
+
+    console.log(arrayConmebol);
+}
+
+//Quitar algun pais
+
+function bajaPais (){
+    let pais = prompt("Ingrese el pais del menu desea eliminar: ");
+    let conmebol = arrayConmebol.find(conmebol => conmebol.pais === pais);
+    let indice = arrayConmebol.indexOf(conmebol);
+    arrayConmebol.splice(indice, 1);
+
+    console.log(arrayConmebol);
+
+}
+
+//Moficiar un grupo
+
+function modificarGrupo (){
+    let pais = prompt("Ingrese nombre del pais de Conmebol que desea modificiar su grupo: ");
+    let conmebol = arrayConmebol.find (conmebol => conmebol.pais === pais);
+    let indice = arrayConmebol.indexOf(conmebol);
+    let primero = prompt ("Debut vs: ");
+    let segundo = prompt ("Segundo patido vs: ");
+    let tercero = prompt ("Tercer partido vs: ");
+    let grupoModificado = new Conmebol (pais, primero, segundo, tercero);
+    arrayConmebol.splice(indice, 1, grupoModificado);
+    console.log(arrayConmebol);
+
+}
+
+//Funcion para salir del programa
+
+function salir() {
+    alert ("Veo que sos de Chile, y no te interesa el mundial");
+}
+
+//Ejecuto el programa
+
+let opcion = menu();
+switch (opcion) {
+    case 1:
+        consultaPais();
         break;
-    case "Bra":
-        alert("Pagarías "+ cuotas + " cuotas de $" + parseFloat (((6500*5.17)*1.18)/cuotas).toFixed(2)+ " reales");
+    case 2:
+        altaPais();
         break;
-    case "Uru":
-        alert("Pagarías "+ cuotas + " cuotas de $" + parseFloat (((6500*40.81)*1.1)/cuotas).toFixed(2)+ " pesos uruguayos");
+    case 3:
+        bajaPais();
         break;
-    case "Ecu":
-        alert("Pagarías "+ cuotas + " cuotas de $" + parseFloat (((6500*1)*1.12)/cuotas).toFixed(2)+ " dolares");
+    case 4:
+        modificarGrupo();
         break;
-    case "Chi":
-        alert("Ay que lastima pero este juego es solo para quienes juegan el mundial");
+    case 5:
+        salir();
         break;
     default:
-        alert ("Por favor, revise la consigna.");
+        alert("Revisa el menu por favor");
         break;
-    
 }
-alert(dolarpaises);
-
-
-
-
 
 
 
